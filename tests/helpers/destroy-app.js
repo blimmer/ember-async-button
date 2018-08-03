9693e5cec9aee6533061a4f004925436135bc34d
Raphael Nunes <raphael@v2v.net>
2018-08-03 11:12:17 -0300

    - Updated to ember version 3.0
    # Conflicts:
    #	.travis.yml
    #	LICENSE.md
    #	README.md
    #	config/ember-try.js
    #	ember-cli-build.js
    #	testem.js
    #	tests/.eslintrc.js
    #	tests/dummy/app/app.js
    #	tests/dummy/app/templates/application.hbs
    #	tests/helpers/destroy-app.js
    #	tests/helpers/module-for-acceptance.js
    #	tests/helpers/start-app.js

--- tests/helpers/destroy-app.js
+++
@@ -1,7 +0,0 @@
-import { run } from '@ember/runloop';
-import { assertionCleanup } from '../assertions';
-
-export default function destroyApp(application) {
-  run(application, 'destroy');
-  assertionCleanup(application);
-}
